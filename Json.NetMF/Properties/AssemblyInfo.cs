2cacfb0bd836797d3f30ced71e7810abc1829d27
Rob Tierolff <teh@borreh.nl>
2017-05-12 01:16:36 +0200

    Moved Nuget packaging, fixed files to do Mono

--- Json.NetMF/Properties/AssemblyInfo.cs
+++ Json.NetMF/Properties/AssemblyInfo.cs
@@ -1,25 +1,27 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Json.NetMF")]
-[assembly: AssemblyDescription("JSON Serializer and Deserializer library for the .NET Micro Framework")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Mike Jones, Matt Weimer")]
-[assembly: AssemblyProduct("Json.NetMF")]
-[assembly: AssemblyCopyright("Copyright © Mike Jones, Matt Weimer 2011-2013")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+// Information about this assembly is defined by the following attributes.
+// Change them to the values specific to your project.
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-[assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyTitle ("Json")]
+[assembly: AssemblyDescription ("")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("")]
+[assembly: AssemblyProduct ("")]
+[assembly: AssemblyCopyright ("rob")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCulture ("")]
+
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+
+[assembly: AssemblyVersion ("1.0.*")]
+
+// The following attributes are used to specify the signing key for the assembly,
+// if desired. See the Mono documentation for more information about signing.
+
+//[assembly: AssemblyDelaySign(false)]
+//[assembly: AssemblyKeyFile("")]
+
--- Json.NetMF/StringExtensions.cs
+++ Json.NetMF/StringExtensions.cs
@@ -1,26 +1,25 @@
 ﻿// Source code is modified from Mike Jones's JSON Serialization and Deserialization library (https://www.ghielectronics.com/community/codeshare/entry/357)
 
 using System;
-using Microsoft.SPOT;
 using System.Collections;
 
-namespace Json.NETMF
+namespace SimpleJson.Transcoder
 {
 	internal static class StringExtensions
 	{
-        public static bool EndsWith(this string s, string value)
-        {
-            return s.IndexOf(value) == s.Length - value.Length;
-        }
+		public static bool EndsWith (this string s, string value)
+		{
+			return s.IndexOf (value) == s.Length - value.Length;
+		}
 
-        public static bool StartsWith(this string s, string value)
-        {
-            return s.IndexOf(value) == 0;
-        }
+		public static bool StartsWith (this string s, string value)
+		{
+			return s.IndexOf (value) == 0;
+		}
 
-        public static bool Contains(this string s, string value)
-        {
-            return s.IndexOf(value) >= 0;
-        }
+		public static bool Contains (this string s, string value)
+		{
+			return s.IndexOf (value) >= 0;
+		}
 	}
 }
